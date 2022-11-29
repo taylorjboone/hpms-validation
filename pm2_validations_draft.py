8cba1c9cc356c8d93ebad92184449a7dc1c39e82
KamenRiderRyuki <geekdomfan@yahoo.com>
2022-11-29 08:18:06 -0500

    updated pm2_validations

--- pm2_validations_draft.py
+++ pm2_validations_draft.py
@@ -1,6 +1,6 @@
 import pandas as pd
-input_file = r'G:\.shortcut-targets-by-id\129JU9Dw85cayEM9V_oKEkrAogePup1hS\hpms_website_project\WV_DOT_HPMS_2022_June_Submission\ril_7_6_2022.xlsx'
-df = pd.read_excel(input_file, usecols=['RouteID', 'BMP', 'EMP', '65_STATE_FUNCTIONAL_CLASS', '20_FACILITY', 'Label', '39_OWNERSHIP', '83_URBAN_CODE', '36_NHS', '115_STRAHNET', '17_DES_TRUCK_ROUTE'])
+input_file = 'ril_7_6_2022.xlsx'
+df = pd.read_excel(input_file, usecols=['RouteID', 'BMP', 'EMP', '65_STATE_FUNCTIONAL_CLASS','30_IRI_VALUE','21_FAULTING','14_CRACKING_PERCENT','52_RUTTING'])
 print('Dataframe loaded')
 
 def pm2_spatial_join(df):
@@ -16,17 +16,14 @@ def pm2_spatial_join(df):
             df['section_length_errors'] = 'More than 0.11'
         else:
             return df['section_length_errors']
-    for rid in df['routeid']:
-        if rid == rid +1:
-            
-            
-            
-        
-            
-            
-            
-            print('meh')
+    # for index,row in df.iterrows():
+    #     print(row)
+    #     if ((row['IRI_VALUE'] != row['FAULTING']) and (row['IRI_VALUE'] != row['CRACKING_PERCENT']) and (row['IRI_VALUE'] != row['RUTTING'])):
+    #         print(row['RouteID'],row['BMP'],row['EMP'])
+    errors = df.loc[((df['30_IRI_VALUE'] != df['21_FAULTING']) & (df['30_IRI_VALUE'] != df['14_CRACKING_PERCENT']) & (df['30_IRI_VALUE'] != df['52_RUTTING']))]
+    return errors.to_excel('pm2_errors.xlsx',index=False)
 
+group = pm2_spatial_join(df)
 
         
 
