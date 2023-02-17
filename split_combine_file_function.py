98f10e3e907617463ee5458947eb1db987001f65
KamenRiderRyuki <geekdomfan@yahoo.com>
2023-02-17 12:40:05 -0500

    added create base file functionality
    Added lines of code that creates a base file to be combined on.

--- split_combine_file_function.py
+++ split_combine_file_function.py
@@ -1,16 +1,19 @@
 import os
 from os import listdir
-from os.path import isfile, join 
+from os.path import isfile, join
+import pandas as pd 
 
  
 def split_combine(mypath):
+    df = pd.DataFrame(columns=['Year_Record','State_Code','RouteID','BMP','EMP','Data_Item','Section_Length','Value_Numeric','Value_Text','Value_Date','Comments'])
+    df.to_csv('C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\base_file.csv')
     onlyfiles = [os.path.join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f))]
     for split_file in onlyfiles:
         # print(split_file)
-        prefix = split_file.replace("C:\PythonTest\Voltron\hpms_data_items_2021\\test\\DataItem","").split(".")[0]
-        prefix2 = prefix.replace("C:\PythonTest\Voltron\hpms_data_items_2021\\test\\","")
+        prefix = split_file.replace("C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\test\\DataItem","").split(".")[0]
+        prefix2 = prefix.replace("C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\test\\","")
         print(prefix2)
-        command = f'lrsops split -b C:\PythonTest\Voltron\\base_file.csv -s {split_file} -c "Data_Item,Value_Numeric" --prefix "{prefix2}" -o C:\PythonTest\Voltron\\base_file.csv' 
+        command = f'lrsops split -b C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\base_file.csv -s {split_file} -c "Data_Item,Value_Numeric" --prefix "{prefix2}" -o C:\PythonTest\Voltron\\base_file.csv' 
         print(command)            
         os.system(command)
 
