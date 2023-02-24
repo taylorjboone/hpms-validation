29e49228ee897615e343421382f1d00eeaa99a82
KamenRiderRyuki <geekdomfan@yahoo.com>
2023-02-24 13:59:10 -0500

    Updated Split combine
    Added lines of code that will allow the auto generation of base file which is then used to join with HPMS Data Items.

--- split_combine_file_function.py
+++ split_combine_file_function.py
@@ -8,14 +8,17 @@ def split_combine(mypath):
     df = pd.DataFrame(columns=['Year_Record','State_Code','RouteID','BMP','EMP','Data_Item','Section_Length','Value_Numeric','Value_Text','Value_Date','Comments'])
     df.to_csv('C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\base_file.csv')
     onlyfiles = [os.path.join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f))]
-    for split_file in onlyfiles:
-        # print(split_file)
-        prefix = split_file.replace("C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\test\\DataItem","").split(".")[0]
-        prefix2 = prefix.replace("C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\test\\","")
-        print(prefix2)
-        command = f'lrsops split -b C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\base_file.csv -s {split_file} -c "Data_Item,Value_Numeric" --prefix "{prefix2}" -o C:\PythonTest\Voltron\\base_file.csv' 
+    for split_file in onlyfiles[-6:]:
+        print(split_file)
+        df = pd.read_csv(split_file,sep='|')
+        print(len(df.axes[1]))
+        prefix = split_file.replace("C:\PythonTest\Voltron\district_chrystal_report_website\hpms-validation\hpms_data_items\data_items\DataItem","").split(".")[0]
+        print(prefix)
+        prefix2 = prefix.replace("C:\PythonTest\Voltron\district_chrystal_report_website\hpms-validation\hpms_data_items\data_items","")
+        # print(prefix2)
+        command = f'lrsops split -b C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\base_file.csv -s {split_file} -c "Data_Item,Value_Numeric" --prefix "{prefix2}" -o C:\\PythonTest\\Voltron\\district_chrystal_report_website\\hpms-validation\\base_file.csv' 
         print(command)            
         os.system(command)
 
 
-split_combine(mypath='C:\PythonTest\Voltron\hpms_data_items_2021\\test')
+split_combine(mypath='C:\\PythonTest\Voltron\\district_chrystal_report_website\\hpms-validation\\hpms_data_items\\data_items')
