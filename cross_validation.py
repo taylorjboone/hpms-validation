b9d8c9034a88777cde2f3503ad88fcbef5715516
taylorjboone <taylorjamesboone@gmail.com>
2023-03-01 14:37:20 -0500

    pavement data creation script and cross validation class

--- cross_validation.py
+++ cross_validation.py
@@ -70,90 +70,98 @@ def get_f_system(value):
     elif value in [9, 19]:
         return int(7)
 
-def load_defaults(df):
-    # Standardizes columns names 
-    df.rename(columns=cols_dict, inplace=True)
-    cols = df.columns.tolist()
 
-    if not 'Supp_Code' in cols:
-        df['Supp_Code'] = df['RouteID'].str[9:11]
+class Cross_Validation():
+    def __init__(self, df):
+        self.df = df
 
-    # If RouteNumber column is missing, adds and populates RouteNumber pulled from RouteID
-    if not 'RouteNumber' in cols:
-        df['RouteNumber'] = df['RouteID'].str[3:7]
-        df['RouteNumber'] = df['RouteNumber'].map(lambda x: x.lstrip('0'))
-        print('Added RouteNumber column')
+    def load_defaults(self):
+        df=self.df
+        # Standardizes columns names 
+        df.rename(columns=cols_dict, inplace=True)
+        cols = df.columns.tolist()
 
-    # If Sign System column is missing, adds and populates sign system pulled from RouteID
-    if not 'RouteSigning' in cols:
-        df['RouteSigning'] = df['RouteID'].str[2]
-        print('Added RouteSigning column')
+        if not 'Supp_Code' in cols:
+            df['Supp_Code'] = df['RouteID'].str[9:11]
 
-    # Converts 1-19 F System to FHWA 1-7 F System
-    if not 'F_SYSTEM' in cols:
-        df['F_SYSTEM'] = df['FUNCTIONAL_CLASS'].map(lambda x: get_f_system(x))
-        print('Added State Functional Class column')
+        # If RouteNumber column is missing, adds and populates RouteNumber pulled from RouteID
+        if not 'RouteNumber' in cols:
+            df['RouteNumber'] = df['RouteID'].str[3:7]
+            df['RouteNumber'] = df['RouteNumber'].map(lambda x: x.lstrip('0'))
+            print('Added RouteNumber column')
 
-    # If RouteQualifier column is missing, adds and populates RouteQualifier pulled from RouteID
-    qualifier_dict = {'00':1,'01':2, '02':1, '03':5, '04':1, '05':1, '06':1, '07':1, '08':3, '09':3, '10':3, '11':3, '12':3, '13':9, '14':4, '15':6, '16':10, '17':10, '18':10, '19':10, '20':1, '21':10, '22':10, '23':10, '24':7, '25':10, '26':10, '27':10, '28':10, '51':10, '99':10}
-    if not 'RouteQualifier' in cols:
-        df['RouteQualifier'] = df['RouteID'].str[9:11]
-        df['RouteQualifier'] = df['RouteQualifier'].map(lambda x: qualifier_dict[x])
+        # If Sign System column is missing, adds and populates sign system pulled from RouteID
+        if not 'RouteSigning' in cols:
+            df['RouteSigning'] = df['RouteID'].str[2]
+            print('Added RouteSigning column')
 
-    # Creates Dir through lanes from existing events
-    if not 'Dir_Through_Lanes' in cols:
-        df['Dir_Through_Lanes'] = ''
-        df['Dir_Through_Lanes'].loc[((df['97_ORIG_SURVEY_DIRECTION'].notna()) & (df['97_ORIG_SURVEY_DIRECTION'] == '0'))] = df['PEAK_LANES']
-        df['Dir_Through_Lanes'].loc[((df['97_ORIG_SURVEY_DIRECTION'].notna()) & (df['97_ORIG_SURVEY_DIRECTION'].isin(['1','A'])))] = df['COUNTER_PEAK_LANES']
+        # Converts 1-19 F System to FHWA 1-7 F System
+        if not 'F_SYSTEM' in cols:
+            df['F_SYSTEM'] = df['FUNCTIONAL_CLASS'].map(lambda x: get_f_system(x))
+            print('Added State Functional Class column')
 
-    return df
+        # If RouteQualifier column is missing, adds and populates RouteQualifier pulled from RouteID
+        qualifier_dict = {'00':1,'01':2, '02':1, '03':5, '04':1, '05':1, '06':1, '07':1, '08':3, '09':3, '10':3, '11':3, '12':3, '13':9, '14':4, '15':6, '16':10, '17':10, '18':10, '19':10, '20':1, '21':10, '22':10, '23':10, '24':7, '25':10, '26':10, '27':10, '28':10, '51':10, '99':10}
+        if not 'RouteQualifier' in cols:
+            df['RouteQualifier'] = df['RouteID'].str[9:11]
+            df['RouteQualifier'] = df['RouteQualifier'].map(lambda x: qualifier_dict[x])
 
+        # Creates Dir through lanes from existing events
+        if not 'Dir_Through_Lanes' in cols:
+            df['Dir_Through_Lanes'] = ''
+            df['Dir_Through_Lanes'].loc[((df['97_ORIG_SURVEY_DIRECTION'].notna()) & (df['97_ORIG_SURVEY_DIRECTION'] == '0'))] = df['PEAK_LANES']
+            df['Dir_Through_Lanes'].loc[((df['97_ORIG_SURVEY_DIRECTION'].notna()) & (df['97_ORIG_SURVEY_DIRECTION'].isin(['1','A'])))] = df['COUNTER_PEAK_LANES']
 
-def inventory_spatial_join(df):
-    # Creates a column for each rule, outputs a False for each row that doesn't pass the rule for a column
-    spatial_join_checks = {
-        'sji01': ((df['F_SYSTEM'].notna()) & (df['RouteID'].isin(arnold['RouteID']))),
-        'sji02': ((df['FACILITY_TYPE'].notna()) & (df['RouteID'].isin(arnold['RouteID']))),
-        'sji03': ((df['OWNERSHIP'].notna()) & (df['RouteID'].isin(arnold['RouteID']))),
-        'sji04': ((df['URBAN_ID'].notna()) & (df['RouteID'].isin(arnold['RouteID']))),
-        'sji05': (((df['FACILITY_TYPE'].notna()) & (df['F_SYSTEM'].notna())) | df['FACILITY_TYPE'].isna()),
-        'sji06': (((df['F_SYSTEM'].notna()) & (df['FACILITY_TYPE'].notna())) | df['F_SYSTEM'].isna()),
-        'sji07': (((df['F_SYSTEM'] == 1) & (df['FACILITY_TYPE'].isin([1,2])) & (df['RouteNumber'].notna())) | (df['F_SYSTEM'] != 1)),
-        'sji08': (((df['F_SYSTEM'] == 1) & (df['NHS'] == 1)) | (df['F_SYSTEM'] != 1)),
-        'sji09': (((df['RouteNumber'].notna()) & (df['RouteSigning'].notna())) | (df['RouteNumber'].isna())),
-        'sji10': (((df['RouteNumber'].notna()) & (df['RouteQualifier'].notna())) | (df['RouteNumber'].isna())),
-        'sji11': (((df['F_SYSTEM'] == 1) & (df['STRAHNET'] == 1)) | (df['F_SYSTEM'] != 1)),
-        'sji12': (((df['STRAHNET'].isin([1,2])) & (df['NHS'] == 1)) | (~df['STRAHNET'].isin([1,2]))),
-        'sji13': (((df['F_SYSTEM'] == 1) & (df['NN'] == 1)) | (df['F_SYSTEM'] != 1))
-    }
-
-    tmp = df.copy()
-    for k,v in spatial_join_checks.items():
-        tmp[k] = v
-    tmp2 = pd.DataFrame()
-    for rule in spatial_join_checks.keys():
-        tmp2 = tmp2.append(tmp[tmp[rule] == False])
-    return tmp2.drop_duplicates()
+        return df
 
 
-def traffic_spatial_join(df):
-    spatial_join_checks = {
-        'sjt01': ((df['AADT_SINGLE_UNIT'].isna()) | (df['AADT_SINGLE_UNIT'] > (0.4 * df['AADT']))),
-        'sjt02': ((df['AADT_SINGLE_UNIT'].isna()) | (((df['AADT'] > 500) & (df['AADT_SINGLE_UNIT'] > 0)) | (df['AADT'] <= 500))),
-        'sjt03': ((df['AADT_SINGLE_UNIT'].isna()) | ((df['AADT_SINGLE_UNIT'] + df['AADT_COMBINATION']) < (0.8 * df['AADT']))),
-        'sjt04': ((df['AADT_SINGLE_UNIT'].isna()) | (((df['AADT_SINGLE_UNIT'] * 0.01) < (df['AADT'] * (df['PCT_DH_SINGLE_UNIT'] * .01))) & ((df['AADT'] * (df['PCT_DH_SINGLE_UNIT'] * .01)) < (df['AADT_SINGLE_UNIT'] * 0.5)))),
-        'sjt05': ((df['PCT_DH_SINGLE_UNIT'].isna()) | ((df['PCT_DH_SINGLE_UNIT'] > 0) & (df['PCT_DH_SINGLE_UNIT'] < 25))),
-        'sjt06': ((df['PCT_DH_SINGLE_UNIT'].isna()) | (((df['AADT_SINGLE_UNIT'] < 50) & (df['PCT_DH_SINGLE_UNIT'] == 0)) | (df['PCT_DH_SINGLE_UNIT'] != 0))),
-        'sjt07': ((df['AADT_COMBINATION'].isna()) | (df['AADT_COMBINATION'] < (0.4 * df['AADT'])))
-    }
+    def inventory_spatial_join(self):
+        df = self.df
+        # Creates a column for each rule, outputs a False for each row that doesn't pass the rule for a column
+        spatial_join_checks = {
+            'sji01': ((df['F_SYSTEM'].notna()) & (df['RouteID'].isin(arnold['RouteID']))),
+            'sji02': ((df['FACILITY_TYPE'].notna()) & (df['RouteID'].isin(arnold['RouteID']))),
+            'sji03': ((df['OWNERSHIP'].notna()) & (df['RouteID'].isin(arnold['RouteID']))),
+            'sji04': ((df['URBAN_ID'].notna()) & (df['RouteID'].isin(arnold['RouteID']))),
+            'sji05': (((df['FACILITY_TYPE'].notna()) & (df['F_SYSTEM'].notna())) | df['FACILITY_TYPE'].isna()),
+            'sji06': (((df['F_SYSTEM'].notna()) & (df['FACILITY_TYPE'].notna())) | df['F_SYSTEM'].isna()),
+            'sji07': (((df['F_SYSTEM'] == 1) & (df['FACILITY_TYPE'].isin([1,2])) & (df['RouteNumber'].notna())) | (df['F_SYSTEM'] != 1)),
+            'sji08': (((df['F_SYSTEM'] == 1) & (df['NHS'] == 1)) | (df['F_SYSTEM'] != 1)),
+            'sji09': (((df['RouteNumber'].notna()) & (df['RouteSigning'].notna())) | (df['RouteNumber'].isna())),
+            'sji10': (((df['RouteNumber'].notna()) & (df['RouteQualifier'].notna())) | (df['RouteNumber'].isna())),
+            'sji11': (((df['F_SYSTEM'] == 1) & (df['STRAHNET'] == 1)) | (df['F_SYSTEM'] != 1)),
+            'sji12': (((df['STRAHNET'].isin([1,2])) & (df['NHS'] == 1)) | (~df['STRAHNET'].isin([1,2]))),
+            'sji13': (((df['F_SYSTEM'] == 1) & (df['NN'] == 1)) | (df['F_SYSTEM'] != 1))
+        }
 
-    tmp = df.copy()
-    for k,v in spatial_join_checks.items():
-        tmp[k] = v
-    tmp2 = pd.DataFrame()
-    for rule in spatial_join_checks.keys():
-        tmp2 = tmp2.append(tmp[tmp[rule] == False])
-    return tmp2.drop_duplicates()
+        tmp = df.copy()
+        for k,v in spatial_join_checks.items():
+            tmp[k] = v
+        tmp2 = pd.DataFrame()
+        for rule in spatial_join_checks.keys():
+            tmp2 = tmp2.append(tmp[tmp[rule] == False])
+        return tmp2.drop_duplicates()
+
+
+    def traffic_spatial_join(self):
+        df = self.df
+        spatial_join_checks = {
+            'sjt01': ((df['AADT_SINGLE_UNIT'].isna()) | (df['AADT_SINGLE_UNIT'] > (0.4 * df['AADT']))),
+            'sjt02': ((df['AADT_SINGLE_UNIT'].isna()) | (((df['AADT'] > 500) & (df['AADT_SINGLE_UNIT'] > 0)) | (df['AADT'] <= 500))),
+            'sjt03': ((df['AADT_SINGLE_UNIT'].isna()) | ((df['AADT_SINGLE_UNIT'] + df['AADT_COMBINATION']) < (0.8 * df['AADT']))),
+            'sjt04': ((df['AADT_SINGLE_UNIT'].isna()) | (((df['AADT_SINGLE_UNIT'] * 0.01) < (df['AADT'] * (df['PCT_DH_SINGLE_UNIT'] * .01))) & ((df['AADT'] * (df['PCT_DH_SINGLE_UNIT'] * .01)) < (df['AADT_SINGLE_UNIT'] * 0.5)))),
+            'sjt05': ((df['PCT_DH_SINGLE_UNIT'].isna()) | ((df['PCT_DH_SINGLE_UNIT'] > 0) & (df['PCT_DH_SINGLE_UNIT'] < 25))),
+            'sjt06': ((df['PCT_DH_SINGLE_UNIT'].isna()) | (((df['AADT_SINGLE_UNIT'] < 50) & (df['PCT_DH_SINGLE_UNIT'] == 0)) | (df['PCT_DH_SINGLE_UNIT'] != 0))),
+            'sjt07': ((df['AADT_COMBINATION'].isna()) | (df['AADT_COMBINATION'] < (0.4 * df['AADT'])))
+        }
+
+        tmp = df.copy()
+        for k,v in spatial_join_checks.items():
+            tmp[k] = v
+        tmp2 = pd.DataFrame()
+        for rule in spatial_join_checks.keys():
+            tmp2 = tmp2.append(tmp[tmp[rule] == False])
+        return tmp2.drop_duplicates()
 
 
 # input_file = 'lrs_data/lrs_dump_12-05-22.csv'
---
+++ pavement_data_item_creation.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+data_cols = ['RUT_MEAN', 'Fault_Avg', 'Percent_Cracking', 'IRI_MEAN']
+route_cols = ['ROUTEID', 'BEG_MP', 'END_MP', 'Date']
+rename_dict = {
+    'ROUTEID': 'RouteID',
+    'BEG_MP': 'BeginPoint',
+    'END_MP':'EndPoint',
+    'RUT_MEAN':'RUTTING',
+    'Fault_Avg':'FAULTING',
+    'Percent_Cracking':'CRACKING_PERCENT',
+    'IRI_MEAN': 'IRI',
+    'Date': 'ValueDate'
+}
+data_items = ['RUTTING', 'FAULTING', 'CRACKING_PERCENT', 'IRI']
+master = pd.read_excel('hpms_data_items/pavement/2023_submission_pavement_data.xlsx', usecols=data_cols + route_cols)
+master.rename(columns=rename_dict, inplace=True)
+
+
+def load_defaults(df):
+    df['BeginDate'] = '12/31/2021'
+    df['StateID'] = '54'
+    df['Comments'] = ''
+    return df
+
+
+def sort_cols(df):
+    df = df[['BeginDate', 'StateID', 'RouteID', 'BeginPoint', 'EndPoint', 'DataItem', 'ValueNumeric', 'ValueText', 'ValueDate', 'Comments']]
+    return df
+
+
+def create_data_item(df, data_item):
+    df = df[['RouteID', 'BeginPoint', 'EndPoint', f'{data_item}', 'ValueDate']]
+    df = load_defaults(df)
+    df.rename(columns={f'{data_item}':'ValueNumeric'}, inplace=True)
+    df['DataItem'] = f'{data_item}'
+    df['ValueText'] = ''
+    df = sort_cols(df)
+    return df
+
+data_item_dict = {}
+for i in data_items:
+    data_item_dict[i] = create_data_item(master, i)
+
+for k,v in data_item_dict.items():
+    print(k, '\n', v, '\n\n\n')
+
+
+
+
+# def create_rutting(df):
+#     df = df[['RouteID', 'BeginPoint', 'EndPoint', 'RUTTING', 'ValueDate']]
+#     df.rename(columns={'RUTTING': 'ValueNumeric'}, inplace=True)
+#     df['DataItem'] = 'RUTTING'
+#     df['ValueText'] = ''
+#     return df
+
+
+# def create_faulting(df):
+#     df = df[['RouteID', 'BeginPoint', 'EndPoint', 'FAULTING', 'ValueDate']]
+#     df.rename(columns={'FAULTING': 'ValueNumeric'}, inplace=True)
+#     df['DataItem'] = 'FAULTING'
+#     df['ValueText'] = ''
+#     return df
+
+
+
