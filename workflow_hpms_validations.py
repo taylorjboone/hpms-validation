6e6dc33874629ec7cd54a6a5c330b5133857192d
taylorjboone <taylorjamesboone@gmail.com>
2023-06-15 08:38:13 -0400

    REPO CLEANUP

--- workflow_hpms_validations.py
+++
@@ -1,4 +0,0 @@
-#step 1: read the files from folder and return them as a list
-#step 2: itterate through the list using dictionary with key being file and domain validation function being the value
-#step 4: concat the files into one file then perform cross validation,full spatial join, pm2 join and traffic join 
-#step 5: for each validation steps, create error file detailing the errors found in each validation
