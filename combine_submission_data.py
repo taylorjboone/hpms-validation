43d25c79b1e862c6e051fbb034dfc2c722b61e5a
taylorjboone <taylorjamesboone@gmail.com>
2023-06-16 12:16:35 -0400

    working combine script for submitted dataitems

--- combine_submission_data.py
+++ combine_submission_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import json
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
@@ -20,6 +21,8 @@ for file in file_list:
     print(f'title: {file["title"]}, id: {file["id"]}')
 
 
+fixed_files = drive.ListFile({'q': "'1HKojRkG3Da630GlvYvRevf2WV5hyVZPJ' in parents and trashed=False"}).GetList()
+fixed_files = [file for file in fixed_files if (file['mimeType'] == 'text/csv') & (file['title'][0:8] == 'DataItem')]
 
 abril_submission = drive.ListFile({'q': "'1p3-83rG2AndBFqa4JXBB6nLPb7zx8FgW' in parents and trashed=False"}).GetList()
 abril_submission = [file for file in abril_submission if (file['mimeType'] == 'text/csv') & (file['title'][0:8] == 'DataItem')]
@@ -33,48 +36,89 @@ june_submission = [file for file in june_submission if (file['mimeType'] == 'tex
 traffic_data = drive.ListFile({'q': "'1p3-83rG2AndBFqa4JXBB6nLPb7zx8FgW' in parents and trashed=False"}).GetList()
 traffic_data = [file for file in traffic_data if (file['mimeType'] == 'text/csv') & (file['title'][0:8] == 'DataItem')]
 
-fixed_files = drive.ListFile({'q': "'1HKojRkG3Da630GlvYvRevf2WV5hyVZPJ' in parents and trashed=False"}).GetList()
-fixed_files = [file for file in fixed_files if (file['mimeType'] == 'text/csv') & (file['title'][0:8] == 'DataItem')]
+district_data = drive.ListFile({'q': "'1cSF7v6WhTS7hWIddER37yaZppUbO3j62' in parents and trashed=False"}).GetList()
+district_data = [file for file in district_data if (file['mimeType'] == 'text/csv')]
 
 
-dataset_list = [abril_submission, pavement_data, june_submission, traffic_data, fixed_files]
+
+
+dataset_list = [fixed_files, june_submission, traffic_data, abril_submission, pavement_data, district_data]
+unique_titles = []
 unique_items = []
 for dataset in dataset_list:
     for i in dataset:
-        if not i['title'] in unique_items:
-            unique_items.append(i['title'])
+        if not i['title'] in unique_titles:
+            unique_titles.append(i['title'])
+            unique_items.append(i)
+        else:
+            print(f'duplicated item: {i["parents"][0]["id"]}')
 
-print(f'Number of Data Items: {len(unique_items)}')
+num_files = len(unique_items)
+print(f'Number of Data Items: {num_files}')
 
 
 pos = 0
-total = [i['title'] for i in fixed_files] + [i['title'] for i in june_submission if not i['title'] in [j['title'] for j in fixed_files]]
 print('total')
-[print(i) for i in sorted(total)]
-newlist = []
+[print(i) for i in sorted(unique_titles)]
+operations = []
 
 if not os.path.exists('tmp'):
     os.mkdir('tmp')
 
-for dataset in dataset_list:
-    for file in dataset:
-        print(file['title'])
-        download = drive.CreateFile({'id':file['id']})
-        download.GetContentFile('example.csv')
-        df = pd.read_csv('example.csv', sep='|')
+basedf = pd.DataFrame(columns=['ROUTEID', 'BMP', 'EMP'])
+basedf.to_csv('all_submission_data.csv')
 
-        if 'Data_Item' in df.columns.tolist():
-            data_item = df['Data_Item'].iloc[0]
-            df.rename(columns={'Value_Numeric': f'{data_item}', 'Value_Date':f'{data_item}_VALUE_DATE', 'Value_Text':f'{data_item}_VALUE_TEXT', 'Begin_Point':'BMP', 'End_Point':'EMP', 'Route_ID':'ROUTEID'})
-        elif 'DataItem' in df.columns.tolist():
-            data_item = df['DataItem'].iloc[0]
-            df.rename(columns={'ValueNumeric': f'{data_item}', 'ValueDate':f'{data_item}_VALUE_DATE', 'ValueText':f'{data_item}_VALUE_TEXT', 'BeginPoint':'BMP', 'EndPoint':'EMP', 'RouteID':'ROUTEID'})
-        else:
-            print('COLUMN FORMAT ERROR')
-        
-        cols = [f'{data_item}', f'{data_item}_VALUE_DATE', f'{data_item}_VALUE_TEXT']
-        filename = f'tmp/{data_item}.csv'
-        df.to_csv(filename)
+for file in unique_items:
+    print(file['title'])
+    download = drive.CreateFile({'id':file['id']})
+    download.GetContentFile('example.csv')
+    df = pd.read_csv('example.csv', sep='|')
+
+    if 'Data_Item' in df.columns.tolist():
+        data_item = df['Data_Item'].iloc[0].upper()
+        data_item = data_item
+        df.rename(columns={'Value_Numeric': f'{data_item}', 'Value_Date':f'{data_item}_VALUE_DATE', 'Value_Text':f'{data_item}_VALUE_TEXT', 'Begin_Point':'BMP', 'End_Point':'EMP', 'Route_ID':'ROUTEID'})
+    elif 'DataItem' in df.columns.tolist():
+        data_item = df['DataItem'].iloc[0]
+        df.rename(columns={'ValueNumeric': f'{data_item}', 'ValueDate':f'{data_item}_VALUE_DATE', 'ValueText':f'{data_item}_VALUE_TEXT', 'BeginPoint':'BMP', 'EndPoint':'EMP', 'RouteID':'ROUTEID'})
+    else:
+        print('COLUMN FORMAT ERROR')
+    
+    cols = [f'{data_item}', f'{data_item}_VALUE_DATE', f'{data_item}_VALUE_TEXT']
+    filename = f'tmp/{data_item}.csv'
+    df.to_csv(filename)
+
+    if pos == 0:
+        op = {
+            "operation":"overlay",
+            "base_file":'all_submission_data.csv',
+            "split_file":filename,
+            "split_fields":cols,
+            "out_file":"temp"
+        }
+    elif pos == num_files-1:
+        op = {
+            "operation":"overlay",
+            "base_file":"previous",
+            "split_file":filename,
+            "split_fields":cols,
+            "out_file":"all_submission_data.csv"
+        }   
+    else:
+        op = {
+            "operation":"overlay",
+            "base_file":"previous",
+            "split_file":filename,
+            "split_fields":cols,
+            "out_file":"temp"
+        }
+    operations.append(op)
+    pos+=1
+
+myjson = {'operations':operations}
+with open('myfile.json','w') as f:
+    f.write(json.dumps(myjson))
+os.system('lrsops overlay --operations myfile.json')
 
 
 # for file in june_submission:
