595aa745a30c0a949d3f837bea51585e958539a8
aSeaBacon <taf5307@gmail.com>
2023-07-07 15:39:27 -0400

    New Rules
    Created new rules for domain validations

--- domain_validation_tyler.py
+++ domain_validation_tyler.py
@@ -6,6 +6,9 @@ from openpyxl.utils import get_column_letter
 import warnings
 warnings.filterwarnings("ignore")
 
+URBAN_CODE_list = ['06139','15481','21745','36190','40753','59275','67672','93592','94726', '99998', '99999']
+
+
 class domain_validations():
     
     def __init__(self,df):
@@ -99,34 +102,227 @@ class domain_validations():
         tempDF = tempDF[(tempDF['EMP'].str[0].str.len() > 8) | (tempDF['EMP'].str[1].str.len() > 3)]
         self.df['DRD204'].iloc[tempDF.index.tolist()] = False
 
+    def dre02(self):
+        #URBAN_ID ValueNumeric must be an integer and a valid US Census Urban Code for the applicable state
 
+        print("Running rule DRE02...")
+        self.df['DRE02'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['URBAN_CODE'].notna()]
+        tempDF = tempDF[~tempDF['URBAN_CODE'].isin(URBAN_CODE_list) | tempDF['URBAN_CODE'].str.contains('.', regex=False)]
+        self.df['DRE02'].iloc[tempDF.index.tolist()] = False  
 
+    def dre03(self):
+        #FACILITY_TYPE ValueNumeric must be a valid integer in the range (1;2;4;5;6;7)
 
+        print("Running rule DRE03...")
+        self.df['DRE03'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['FACILITY_TYPE'].notna()]
+        tempDF = tempDF[~tempDF['FACILITY_TYPE'].isin([1,2,4,5,6,7])]
+        self.df['DRE03'].iloc[tempDF.index.tolist()] = False
+
+    def dre04(self):
+        #STRUCTURE_TYPE ValueNumeric must be a valid integer within the range (1-3)
+
+        print("Running rule DRE04...")
+        self.df['DRE04'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['STRUCTURE_TYPE'].notna()]
+        tempDF = tempDF[~tempDF['STRUCTURE_TYPE'].isin([1,2,3])]
+        self.df['DRE04'].iloc[tempDF.index.tolist()] = False
+
+    def dre05(self):
+        #ACCESS_CONTROL ValueNumeric must be a valid integer within the range (1-3)
+
+        print("Running rule DRE05...")
+        self.df['DRE05'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['ACCESS_CONTROL'].notna()]
+        tempDF = tempDF[tempDF['ACCESS_CONTROL'].isin([1,2,3])]
+        self.df['DRE05'].iloc[tempDF.index.tolist()] = False
+
+    def dre06(self):
+        #OWNERSHIP ValueNumeric must be a valid integer within the range (1;2;3;4;11;12;21;25;26;27;31;32;40;50;60;62;63;64;66;67;68;69;70;72;73;74;80)
+
+        print("Running rule DRE06...")
+        self.df['DRE06'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['OWNERSHIP'].notna()]
+        tempDF = tempDF[~tempDF['OWNERSHIP'].isin([1,2,3,4,11,12,21,25,26,27,31,32,40,50,60,62,63,64,66,67,68,69,70,72,73,74,80])]
+        self.df['DRE06'].iloc[tempDF.index.tolist()] = False
                 
+    def dre07(self):
+        #THROUGH_LANES ValueNumeric must be a valid integer > 0 
 
- 
-    # def drd(self):
-    #     print("Running rule DRD...")
-    #     self.df['DRD'] = True
+        print("Running rule DRE07...")
+        self.df['DRE07'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['THROUGH_LANES'].notna()]
+        tempDF = tempDF[(tempDF['THROUGH_LANES'] <= 1) | tempDF[tempDF['THROUGH_LANES'].str.contains(".", regex=False)]]
+        self.df['DRE07'].iloc[tempDF.index.tolist()] = False
+
+    def dre10(self):
+        #PEAK_LANES ValueNumeric must be a valid integer > 0
+
+        print("Running rule DRE10...")
+        self.df['DRE10'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['PEAK_LANES'].notna()]
+        tempDF = tempDF[tempDF['PEAK_LANES'] <= 0]
+        self.df['DRE10'].iloc[tempDF.index.tolist()] = False
+
+    def dre11(self):
+        #COUNTER_PEAK_LANES ValueNumeric must be a valid integer > 0 
+
+        print("Running rule DRE11...")
+        self.df['DRE11'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['COUNTER_PEAK_LANES'].notna()]
+        tempDF = tempDF[tempDF['COUNTER_PEAK_LANES'] <= 0]
+        self.df['DRE11'].iloc[tempDF.index.tolist()] = False
+
+    def dre12(self):
+        #TURN_LANES_R ValueNumeric must be a valid integer within the range (1-6)
+
+        print("Running rule DRE12...")
+        self.df['DRE12'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['TURN_LANES_R'].notna()]
+        tempDF = tempDF[~tempDF['TURN_LANES_R'].isin(range(1,7))]
+        self.df['DRE12'].iloc[tempDF.index.tolist()] = False
+
+    def dre13(self):
+        #TURN_LANES_L ValueNumeric must be a valid integer within the range (1-6)
+
+        print("Running rule DRE13...")
+        self.df['DRE13'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['TURN_LANES_L'].notna()]
+        tempDF = tempDF[~tempDF['TURN_LANES_L'].isin(range(1,7))]
+        self.df['DRE13'].iloc[tempDF.index.tolist()] = False
+
+    def dre14(self):
+        #SPEED_LIMIT ValueNumeric must be a valid integer in the range (0-99)
+
+        print("Running rule DRE14...")
+        self.df['DRE14'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['SPEED_LIMIT'].notna()]
+        tempDF = tempDF[~tempDF['SPEED_LIMIT'].isin(range(0,100))]
+        self.df['DRE14'].iloc[tempDF.index.tolist()] = False
+
+    def dre15(self):
+        #TOLL_ID ValueNumeric must be a valid Toll ID of no more than 15
+        #Assuming this rule means no more than 15 characters, as most toll IDs are > 15
+
+        print("Running rule DRE15...")
+        self.df['DRE15'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['TOLL_ID'].notna()]
+        tempDF = tempDF[tempDF['TOLL_ID'].astype("string").str.len() > 15]
+        self.df['DRE15'].iloc[tempDF.index.tolist()] = False
+
+    def dre21(self):
+        #AADT ValueNumeric must be positive integer in the range (0-600000); 
+        # ValueText must be in (A;B;C;D;E); 
+        # and ValueDate must not be null; and must be in the format YYYY
+        print("Running rule DRE21...")
+        self.df['DRE21'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['AADT'].notna()]
+        tempDF = tempDF[~tempDF['AADT'].isin(range(0,600001))]
+        self.df['DRE21'].iloc[tempDF.index.tolist()] = False
+
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['AADT_VALUE_TEXT'].notna()]
+        tempDF = tempDF[~tempDF['AADT_VALUE_TEXT'].str.contains("[ABCDE]{1}", regex=True)]
+        self.df['DRE21'].iloc[tempDF.index.tolist()] = False
+
+        tempDF = self.df.copy()
+        tempDF = tempDF[~tempDF['AADT_VALUE_DATE'].str.contains("^\d{4}$") | tempDF['AADT_VALUE_DATE'].isna()]
+        self.df['DRE21'].iloc[tempDF.index.tolist()] = False
+
+    def dre22(self):
+        #AADT_SINGLE_UNIT ValueNumeric must be positive integer in the range (0-500000);
+        #  ValueText must be in (A;B;C;D;E)
+
+        print("Running rule DRE22...")
+        self.df['DRE22'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['AADT_SINGLE_UNIT'].notna()]
+        tempDF = tempDF[~tempDF['AADT_SINGLE_UNIT'].isin(range(0,500001))]
+        self.df['DRE22'].iloc[tempDF.index.tolist()] = False
+
+        tempDF = self.df.copy()
+        tempDF = tempDF[~tempDF['AADT_SINGLE_UNIT_VALUE_TEXT'].str.contains("[ABCDE]{1}", regex=True)]
+        self.df['DRE22'].iloc[tempDF.index.tolist()] = False
+
+    def dre23(self):
+        #PCT_DH_SINGLE_UNIT ValueNumeric must be in the range (0-50)
+
+        print("Running rule DRE23...")
+        self.df['DRE23'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['PCT_DH_SINGLE_UNIT'].notna()]
+        tempDF = tempDF[~tempDF['PCT_DH_SINGLE_UNIT'].isin(range(0,51))]
+        self.df['DRE23'].iloc[tempDF.index.tolist()] = False   
+
+    def dre24(self):
+        #AADT_COMBINATION ValueNumeric must be positive integer in the range (0-500000); ValueText must be in (A;B;C;D;E)
+
+        print("Running rule DRE24...")
+        self.df['DRE24'] = True
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['AADT_COMBINATION'].notna()]
+        tempDF = tempDF[~tempDF['AADT_COMBINATION'].isin(range(0,500001))]
+        self.df['DRE24'].iloc[tempDF.index.tolist()] = False   
+
+        tempDF = self.df.copy()
+        tempDF = tempDF[tempDF['AADT_COMBINATION_VALUE_TEXT'].notna()]
+        tempDF = tempDF[~tempDF['AADT_COMBINATION_VALUE_TEXT'].str.contains("[ABCDE]{1}", regex=True)]
+        self.df['DRE24'].iloc[tempDF.index.tolist()] = False 
+
+
+    # def dre(self):
+    #     print("Running rule DRE...")
+    #     self.df['DRE'] = True
     #     tempDF = self.df.copy()
 
-    #     self.df['DRD'].iloc[tempDF.index.tolist()] = False
+    #     self.df['DRE'].iloc[tempDF.index.tolist()] = False
 
     def run(self):
+
         self.drd01()
         self.drd64()
         self.drd65()
         self.drd66()
-        # self.drd72()
+        # self.drd72() Data item not in all submission data
         self.drd202()
         self.drd203()
         self.drd204()
+        self.dre02()
+        self.dre03()
+        self.dre04()
+        self.dre05()
+        self.dre06()
+        self.dre07()
+        self.dre10()
+        self.dre11()
+        self.dre12()
+        self.dre13()
+        self.dre14()
+        self.dre15()
+        self.dre21()
+        self.dre22()
+        self.dre23()
+        self.dre24()
 
 
 
 
 
-df = pd.read_csv("all_submission_data.csv")
+df = pd.read_csv("all_submission_data.csv", dtype={'URBAN_CODE':str, 'AADT_VALUE_DATE':str})
 c = domain_validations(df)
 c.run()
 c.df.to_csv('test_domain_tyler.csv', index=False) 
