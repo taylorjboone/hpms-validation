d448cbef144c1877bc98ba2a5d22b1035df41fae
Dragonstin <nanda.dagostin@gmail.com>
2024-04-03 11:22:57 -0400

    update the Pavement generation
    - Create the pavement_data_item_creation_mod_2024.py
    - Update the CURB conversion to 1-no shoulder
    - Cover the IRI values to be within the accepted range for HPMS
    - Comment the filter to generate full extension data item

---
+++ pavement_data_item_creation_mod_2024.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import numpy as np
+
+
+data_cols = ['RUT_MEAN', 'FAULT_AVG', 'HPMS_Cracking_Percent', 'IRI_MEAN', 'SURF_TYPE', 'SHLD_TYPE']
+route_cols = ['ROADNAME', 'BEG_MP', 'END_MP', 'DATE']
+rename_dict = {
+    'ROADNAME': 'RouteID',
+    'BEG_MP': 'BeginPoint',
+    'END_MP':'EndPoint',
+    'RUT_MEAN':'RUTTING',
+    'FAULT_AVG':'FAULTING',
+    'HPMS_Cracking_Percent':'CRACKING_PERCENT',
+    'IRI_MEAN': 'IRI',
+    'SURF_TYPE': 'SURFACE_TYPE',
+    'SHLD_TYPE': 'SHOULDER_TYPE',
+    'DATE': 'ValueDate'
+}
+
+data_number = {
+    'RUTTING': '50',
+    'FAULTING': '51',
+    'CRACKING_PERCENT': '52',
+    'IRI': '47',
+    'SURFACE_TYPE': '49',
+    'SHOULDER_TYPE': '37'
+}
+
+
+data_items = ['RUTTING', 'FAULTING', 'CRACKING_PERCENT', 'IRI', 'SURFACE_TYPE', 'SHOULDER_TYPE']
+master = pd.read_excel(f'pavement_output_3_4_24\\2023_COMBINED_ROUTES_DATA_ALL_3_4_24.xlsx', usecols=data_cols + route_cols)
+master = master[master['SHLD_TYPE'].notna()]
+# master = master[master['SHLD_TYPE']!='CURB']
+# master = master[master['SHLD_TYPE']!='Curb']
+# master = master[master['IRI_MEAN']!='0.0']
+# master = master[master['IRI_MEAN']!=0.0]
+master['IRI_MEAN'] = master['IRI_MEAN'].round()
+master['FAULT_AVG'] = master['FAULT_AVG'].round(2)
+# master['PERCENT_CRACKING'] = master['PERCENT_CRACKING'].round(2)
+master.rename(columns=rename_dict, inplace=True)
+# master = master[master['RouteID'].str[2] == '1']
+
+def convert_date(x):
+    day,month,year = x.split('/')
+    out = '/'.join([month,day,year])
+    return out
+
+
+def load_defaults(df):
+    df['BeginDate'] = '01/01/2023'
+    df['StateID'] = '54'
+    df['Comments'] = ''
+    df['ValueDate'] = '06/24/2023'
+    return df
+
+
+def sort_cols(df):
+    df = df[['BeginDate', 'StateID', 'RouteID', 'BeginPoint', 'EndPoint', 'DataItem', 'ValueNumeric', 'ValueText', 'ValueDate', 'Comments']]
+    return df
+
+
+def create_data_item(df, data_item):
+    df = df[['RouteID', 'BeginPoint', 'EndPoint', f'{data_item}', 'ValueDate']]
+    df = load_defaults(df)
+    df.rename(columns={f'{data_item}':'ValueNumeric'}, inplace=True)
+    df = df[df['ValueNumeric'] != -1]
+    df['DataItem'] = f'{data_item}'
+    df['ValueText'] = ''
+    df = sort_cols(df)
+    return df
+
+
+data_item_dict = {}
+for i in data_items:
+    data_item_dict[i] = create_data_item(master, i)
+    # --- Fixing the values for IRI, if IRI < 31, set the value to 31. If the value > 399 set the value 399.
+    if(i == 'IRI'):
+        data_item_dict[i]['ValueNumeric'] = np.where(data_item_dict[i]['ValueNumeric'].astype(int) <31, 31, data_item_dict[i]['ValueNumeric'].astype(int))
+        data_item_dict[i]['ValueNumeric'] = np.where(data_item_dict[i]['ValueNumeric'].astype(int) >399, 399, data_item_dict[i]['ValueNumeric'].astype(int))
+
+surf_dict = {'JCP': 3,'CRC':3, 'ASP': 6,'BRI':11,'OTH':11}
+data_item_dict['SURFACE_TYPE']['ValueNumeric'] = data_item_dict['SURFACE_TYPE']['ValueNumeric'].map(lambda x: surf_dict[x])
+
+
+def shoulder_mapper(x):
+    if int(x) == 7:
+        return 1
+    else:
+        return x
+    
+
+
+
+shld_dict = {'COMBO':5,'EARTH':6,'GRAVEL':4,'NONE':1,'NULL':1,'PAVED':2,'Curb':1,'CURB':1}
+data_item_dict['SHOULDER_TYPE']['ValueNumeric'] = data_item_dict['SHOULDER_TYPE']['ValueNumeric'].map(lambda x : shld_dict[x])
+# data_item_dict['SHOULDER_TYPE'] = data_item_dict['SHOULDER_TYPE'].loc[data_item_dict['SHOULDER_TYPE']['ValueNumeric'].astype('string') != '-1']
+
+
+for k,v in data_item_dict.items():
+    print(k, '\n', v, '\n\n\n')
+    v.to_csv(f'pavement_output_3_4_24/DataItem{data_number[k]}_{k}.csv', index=False, sep='|')
+
+
+
+
+# def create_rutting(df):
+#     df = df[['RouteID', 'BeginPoint', 'EndPoint', 'RUTTING', 'ValueDate']]
+#     df.rename(columns={'RUTTING': 'ValueNumeric'}, inplace=True)
+#     df['DataItem'] = 'RUTTING'
+#     df['ValueText'] = ''
+#     return df
+
+
+# def create_faulting(df):
+#     df = df[['RouteID', 'BeginPoint', 'EndPoint', 'FAULTING', 'ValueDate']]
+#     df.rename(columns={'FAULTING': 'ValueNumeric'}, inplace=True)
+#     df['DataItem'] = 'FAULTING'
+#     df['ValueText'] = ''
+#     return df
+
+
+
